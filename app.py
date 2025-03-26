9282120f70f2bc6a7d321780843d1046f7fbdb9c
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-03-26 18:27:00 +0500

    Required Imports: plotly, pandas, pyttsx3, pytz

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ def render_coin_details(coin_id, coin_name, coin_symbol, crypto_data):
 
 #   -----------
 #  Main Function
-#    ---------
+#   -----------
 
 def main():
     if 'authenticated' not in st.session_state:
